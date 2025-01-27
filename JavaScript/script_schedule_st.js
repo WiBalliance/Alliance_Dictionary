c734e75b7cf4ccf44700241f11a02cfa7207cefd
WiBalliance <njkhgwertnmbdr@gmail.com>
2025-01-27 13:14:17 +0900

    Update script_schedule_st.js

--- JavaScript/script_schedule_st.js
+++ JavaScript/script_schedule_st.js
@@ -157,7 +157,7 @@ document.getElementById("copyButton").addEventListener("click", () => {
   
   // 各コピー用テキストを作成
   const tasksToCopy_1 = sortedTasks
-    .filter(task => new Date(task.end) <= now) // 現在時刻より前のタスクを取得
+    .filter(task => new Date(task.start) <= now) // 現在時刻より前のタスクを取得
     .map(task => {
     const taskStartDate = new Date(task.start);
     const taskEndDate = new Date(task.end);
@@ -194,9 +194,29 @@ document.getElementById("copyButton").addEventListener("click", () => {
   })
   .filter(line => line !== null) // null を取り除く
   .join('\n');
+
+  // 各コピー用テキストを作成
+  const tasksToCopy_3 = sortedTasks
+    .filter(task => new Date(task.end) <= now) // 現在時刻より前のタスクを取得
+    .map(task => {
+    const taskStartDate = new Date(task.start);
+    const taskEndDate = new Date(task.end);
+  
+    const isStartDay = taskStartDate >= targetStartDate && taskStartDate <= targetEndDate;
+    const isEndDay = taskEndDate >= targetStartDate && taskEndDate <= targetEndDate;
+    const isMiddleDay = taskStartDate < targetStartDate && taskEndDate > targetEndDate;
+  
+    if (isEndDay) {
+      const taskEndFormattedTime = `${String(taskEndDate.getHours()).padStart(2, '0')}:${String(taskEndDate.getMinutes()).padStart(2, '0')}`;
+      return `~${taskEndFormattedTime} ${task.name}`;
+    }
+    return null; // 空の行として扱う
+  })
+  .filter(line => line !== null) // null を取り除く
+  .join('\n');
   
   // それぞれの結果を連結
-  const finalTasksToCopy = `【防衛中】\n${tasksToCopy_1}\n\n【解放時間】\n${tasksToCopy_2}\n\n`;
+  const finalTasksToCopy = `【防衛中】\n${tasksToCopy_1}\n\n【解放時間】\n${tasksToCopy_2}\n\n【終了】\n${tasksToCopy_3}`;
   // 結果を表示またはコピー
   console.log(tasksToCopy_1);
   console.log(tasksToCopy_2);
