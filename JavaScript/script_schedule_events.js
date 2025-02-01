c708913f92ba3d45d94f62a7cd6b3718f17b566b
WiBalliance <njkhgwertnmbdr@gmail.com>
2025-02-02 02:36:37 +0900

    Update script_schedule_events.js

--- JavaScript/script_schedule_events.js
+++ JavaScript/script_schedule_events.js
@@ -67,7 +67,7 @@ const generateRepeatingTasks = (tasks) => {
 const updateGantt = (showCompleted, nameFilter = '') => {
   const now = new Date();
   const twoWeeksLater = new Date();
-  twoWeeksLater.setDate(now.getDate() + 9); // 現在から9日後の日付
+  twoWeeksLater.setDate(now.getDate() + 7); // 現在から7日後の日付
 
   const filteredTasks = allTasks.filter(task => {
     const start = new Date(task.start);
